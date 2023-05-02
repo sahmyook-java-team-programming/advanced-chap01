c26420b7e32151e2edc6f4430ec545a67d1669e4
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2023-05-02 16:10:06 +0900

    [문제2] 사람 몸무게 구하기

---
+++ src/main/java/org/example/practice/Application2.java
@@ -0,0 +1,18 @@
+package org.example.practice;
+
+public class Application2 {
+    public static void main(String[] args) {
+        // 문제 2
+        // 출제자 : 조동훈
+        /* 사람들 평균 몸무게를 구하는 문제.
+        * 총 4명의 사람에 대한 평균 몸무게를 구하며
+        * 몸무게는 각각 61kg, 66kg, 73kg , 70kg이다.
+        * 각 사람의 몸무게를 변수에 담아 총 무게와 평균 몸무게를 출력하시오
+        *
+        * -- 출력 예시--
+        * 총 몸무게 : 270
+        * 평균 몸무게 : 67.5
+        * */
+
+    }
+}
