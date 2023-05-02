d5617e44ed93253fc28e6f8d5a512e1538825e70
sejin0211 <sejin0211@naver.com>
2023-05-02 16:00:34 +0900

    First Commit

---
+++ src/main/java/org/example/practice/Application1.java
@@ -0,0 +1,9 @@
+package org.example.practice;
+
+public class Application1 {
+    public static void main(String[] args) {
+        // 문제 1
+        // 출제자 : 이세진
+        System.out.println("이세진");
+    }
+}
